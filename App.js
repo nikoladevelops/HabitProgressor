a7111be890c4b4dcf09fc1bcdc8b9d8dd3eadf4b
Nikola P. <nikoladevelops@gmail.com>
2023-10-13 22:31:53 +0300

    Initialize the db and insert test data on start.

--- App.js
+++ App.js
@@ -3,6 +3,14 @@ import { StyleSheet, ScrollView } from 'react-native';
 import Content from './Components/Content.js';
 import Settings from './Components/Settings.js';
 
+import {initDatabase, insertTestData} from "./db/db.js"
+
+// When opening the app, ensure that the database has been created.
+initDatabase(()=>{
+  insertTestData();
+}); 
+
+
 export default function App() {
   return (
     <ScrollView style={styles.container}>
--- db/db.js
+++ db/db.js
@@ -4,9 +4,58 @@ const dbName = "habits.db"
 const db = SQLite.openDatabase(dbName)
 
 // Function to initialize the database and create tables if necessary
-const initDatabase = () => {
+const initDatabase = (callback) => {
     db.transaction((tx) => {
-      tx.executeSql('CREATE TABLE IF NOT EXISTS Habits (id INTEGER PRIMARY KEY AUTOINCREMENT, description TEXT, streakCount INTEGER)');
+      tx.executeSql('CREATE TABLE IF NOT EXISTS Habits (id INTEGER PRIMARY KEY AUTOINCREMENT, description TEXT, streakCount INTEGER, doneToday BOOLEAN)');
+      
+      // Check if a callback is provided before calling it
+      if (typeof callback === 'function') {
+        callback();
+      }
     });
   };
 
+// Inserts test data inside the db (only if the db's Habits is empty)
+const insertTestData = () => {
+  db.transaction((tx) => {
+    // Check if the table is empty
+    tx.executeSql('SELECT COUNT(*) as count FROM Habits', [], (_, results) => {
+      const { count } = results.rows.item(0);
+
+      if (count === 0) {
+        // The table is empty, insert test data
+
+        for (let i = 0; i < 12; i++) {
+          tx.executeSql(
+            "INSERT INTO Habits (description, streakCount, doneToday) VALUES (?,?,?)",
+            [`This is habit number: ${i}`, 0, 0]
+          );
+        }
+      }
+    });
+  });
+};
+
+// Get all habit data for all rows inside the Habits table
+const getAllHabits = (successCallback, errorCallback) => {
+  db.transaction((tx) => {
+    tx.executeSql("SELECT COUNT(*) as count FROM Habits")
+    tx.executeSql(
+      'SELECT * FROM Habits',
+      [],
+      (_, results) => {
+        const habits = results.rows.raw(); // Convert the results to an array of objects
+        successCallback(habits);
+      },
+      (_, error) => {
+        errorCallback(error);
+      }
+    );
+  });
+};
+
+export default{
+  initDatabase,
+  insertTestData,
+  getAllHabits
+}
