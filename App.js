6f725692b0d36301e9b4c500ee851e11d48260e2
Nikola P. <nikoladevelops@gmail.com>
2023-10-26 16:32:45 +0300

    Remove test data generating.

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StyleSheet, ScrollView, View, Text, TouchableOpacity } from 'react-nati
 import AllHabitButtons from './Components/AllHabitButtons.js';
 import Header from "./Components/Header.js"
 import Settings from './Components/Settings.js';
-import {initTables, checkHabitsCountAsync, createManyHabitsAsync, getAllHabitsAsync, dropHabitsAsync} from "./db/db.js"
+import {initTables, getAllHabitsAsync} from "./db/db.js"
 import React,{ useEffect, useState } from 'react';
 
 import AllHabitsContext from './Contexts/AllHabitsContext.js';
@@ -16,29 +16,6 @@ export default function App() {
   useEffect(()=>{
     async function initialize(){
       await initTables()
-      const countHabits = await checkHabitsCountAsync()
-      if (countHabits === 0){
-        const todayDate = new Date().toISOString()
-        const testData = [
-          {
-            description: "Exercise for 30 minutes",
-            streakCount: 3,
-            lastCompletedDate: todayDate,
-          },
-          {
-            description: "Read 20 pages",
-            streakCount: 5,
-            lastCompletedDate: null, // this means it has yet to be marked as completed
-          },
-          {
-            description: "Meditate for 10 minutes",
-            streakCount: 7,
-            lastCompletedDate: todayDate,
-          }
-        ];
-        await createManyHabitsAsync(testData)
-      }
-
       var allHabits = await getAllHabitsAsync()
       setHabitData(allHabits)
       setIsLoading(false)
@@ -52,8 +29,8 @@ export default function App() {
           <Settings/>
           <Header/>
           {isLoading?
-          <View style={{flex:1, minHeight:500, backgroundColor:"white", justifyContent:"center", alignItems:"center"}}>
-            <Text style={{color:'black', fontSize:33}}>Loading...</Text>
+          <View style={{flex:1, minHeight:500, backgroundColor:"#000", justifyContent:"center", alignItems:"center"}}>
+            <Text style={{color:'#fff', fontSize:33}}>Loading...</Text>
           </View> : <AllHabitButtons/>}
         </AllHabitsContext.Provider>
         <StatusBar style="auto" />
@@ -63,6 +40,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor:"#000",
     flex:1
   },
 });
