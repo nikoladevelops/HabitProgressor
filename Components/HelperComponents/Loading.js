63366da8d006c9fe5a01131cc0a3239ededf38cc
Nikola P. <nikoladevelops@gmail.com>
2023-11-01 17:31:01 +0200

    Made helpful components.

---
+++ Components/HelperComponents/Loading.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { View, Text } from "react-native"
+
+const Loading = ()=>{
+    return (
+    <View style={{ flex: 1, minHeight: 500, backgroundColor: "#000", justifyContent: "center", alignItems: "center" }}>
+        <Text style={{ color: '#fff', fontSize: 33 }}>Loading...</Text>
+    </View>
+    )
+}
+
+export default Loading;
---
+++ Components/HelperComponents/NoHabitsFound.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { View, Text } from "react-native"
+
+const NoHabitsFound = ()=>{
+    return(
+        <View style={{ flex: 1, minHeight: 500, backgroundColor: "#000", justifyContent: "center", alignItems: "center" }}>
+            <Text style={{ color: '#fff', fontSize: 23 }}>No habits found.</Text>
+        </View>
+    )
+}
+
+export default NoHabitsFound;
